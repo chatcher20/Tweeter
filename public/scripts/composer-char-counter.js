896ebca3fda608019c0910d16a2dfa4895d80b17
Chris Hatcher <hatcherchris20@gmail.com>
2022-02-02 17:10:52 +0000

    updating tweet field counter to turn red when iinteger is negative.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,18 @@
+
+
 $(document).ready(function() {
   // --- our code goes here ---
+  $("#tweet-text").on('input', function() {
+    
+    
+    let value = $(this).val().length;
+    let counter = 140 - value;
+    console.log(counter);    
+    $(this).next("div").children("output").text(counter);
+
+    if (counter < 0) {
+      $(".counter").css('color', '#da2809');   // referring to class "counter" on line 70 of index.html, not our variable declared on line 21.
+    }
+  })  
 });
 
